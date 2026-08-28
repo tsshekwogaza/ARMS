aa46a619ddb8880575c7fd8537be7c0a0c59c895
Timothy Samuel <timothysamuelshekwogaza@gmail.com>
2026-08-28 16:44:25 +0100

    Prepare Laravel for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,15 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css', 
+                'resources/js/apexchats.js',
+                'resources/js/app.js',
+                'resources/js/image_preview.js',
+                'resources/js/registerMessage.js',
+                'resources/js/share_refer.js',
+                'resources/js/signinMessage.js',
+            ],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
